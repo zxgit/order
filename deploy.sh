ce97d551b06a1d5dac351039ac65eaecc7050441
张欣 <zhangxin@leweikeji.com>
2020-03-08 20:35:13 +0800

    1111111111111111

--- deploy.sh
+++ deploy.sh
@@ -9,37 +9,38 @@
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 
 ### base 函数
-killTomcat()
-{
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
-    fi
-}
-cd $PROJ_PATH/order
-mvn clean install
+#killTomcat()
+#{
+#    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+ #   echo "tomcat Id list :$pid"
+ #   if [ "$pid" = "" ]
+  #  then
+  #    echo "no tomcat pid alive"
+  #  else
+  #    kill -9 $pid
+  #  fi
+#}
+#cd $PROJ_PATH/order
+#mvn clean install
 
 # 停tomcat
-killTomcat
+#killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+#rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+#rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
+#rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
+#cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+#cd $TOMCAT_APP_PATH/webapps/
+#mv order.war ROOT.war
 
 # 启动Tomcat
-cd $TOMCAT_APP_PATH/
-sh bin/startup.sh
+#cd $TOMCAT_APP_PATH/
+#sh bin/startup.sh
 
 
 
